e5cdd4aecb51a823fc942feff79a9c2754eb57b6
Agustín Sotelo <agustinemiliano22@gmail.com>
2025-04-26 11:21:13 -0300

    Se modifica el nombre del archivo main.py a juego_binario_decimal.py

--- src/juego_binario_decimal.py
+++ src/juego_binario_decimal.py
@@ -2,45 +2,6 @@
 # Muestren un número en binario y desafíen al usuario a adivinar su
 # equivalente decimal, o viceversa, reforzando la conversión entre ambos sistemas.
 
-
-""" Explicación del código de lo que hicimos en la reunión:
-
-Genera un número aleatorio entre 0 y 15.
-
-Lo convierte en binario con una lista predefinida.
-
-Muestran el binario y pide al usuario adivinar el número decimal.
-
-Verifica si la respuesta es correcta o no. """
-
-""" import random
-
-binarios = ["0000", "0001", "0010", "0011",
-    "0100", "0101", "0110", "0111",
-    "1000", "1001", "1010", "1011",
-    "1100", "1101", "1110", "1111"]
-
-bandera = True
-
-while bandera == True:
-    print("JUEGO DE ADIVINANZAS")
-    # Se asigna a la variable "numero" un número aleatorio entre 0 y 15
-    numero = random.randint(0, 15)
-    binario = binarios[numero]
-
-    # Se muestra el binario
-    print(f"Número binario mostrado: {binario}")
-
-    # Pedimos una respuesta
-    respuesta = int(input("Cuál sería el equivalente en decimal?: "))
-
-    if respuesta == numero:
-        print("Es Correcto!")
-        bandera = False
-
-    else:
-        print(f"Incorrecto, la respuesta era: {numero}") """
-
 import random
 
 # Inicializamos las estadísticas
